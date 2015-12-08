7e52620601805b9b2eb1e46b196c6d9c5e55b96b
FranLucchini <flucchini@uc.cl>
2015-12-08 19:41:54 -0300

    merge con channel

--- app/src/main/java/com/mobility42/azurechatr/MemeViewer/MemeCreator.java
+++ app/src/main/java/com/mobility42/azurechatr/MemeViewer/MemeCreator.java
@@ -10,6 +10,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -62,7 +63,6 @@ public class MemeCreator extends Activity implements View.OnClickListener {
     private Spinner font_spinner;
     private Button btnSubmit;
     private EditText textSize, imgText;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -291,17 +291,19 @@ public class MemeCreator extends Activity implements View.OnClickListener {
                 drawView.setDrawingCacheEnabled(true);
 
                 /** ------------------PATH------------------ **/
-                String imgSaved = MediaStore.Images.Media.insertImage(
+                /*String imgSaved = MediaStore.Images.Media.insertImage(
                         getContentResolver(), drawView.getDrawingCache(),
-                        UUID.randomUUID().toString()+".png", "drawing");
+                        UUID.randomUUID().toString() + ".png", "drawing");
+                Uri a = MediaStore.Images.Media.getContentUri();*/
+                addImageToGallery();
 
-                if(imgSaved!=null){
+                if(imgPath!=null){
                     Toast savedToast = Toast.makeText(getApplicationContext(),
-                            "Drawing saved to Gallery! "+imgSaved, Toast.LENGTH_SHORT);
+                            "Drawing saved to Gallery! "+imgPath.getPath(), Toast.LENGTH_SHORT);
                     savedToast.show();
 
 
-                    touchSelection(imgSaved);
+                    touchSelection(imgPath.getPath());
                 }
                 else{
                     Toast unsavedToast = Toast.makeText(getApplicationContext(),
@@ -515,4 +517,39 @@ public class MemeCreator extends Activity implements View.OnClickListener {
         }
     }
 
+    public void addImageToGallery() {
+
+
+        Bitmap bimg = drawView.getDrawingCache();
+
+        File mydir2 = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "memeticame");
+        if (!mydir2.exists()) {
+            mydir2.mkdirs();
+        }
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String date = dateFormat.format(new Date()).toString().replace(" ", "");
+
+        imgPath = new File(mydir2,"meme_"+date+".png");
+        FileOutputStream fOs = null;
+
+        try {
+            fOs = new FileOutputStream(imgPath);
+            bimg.compress(Bitmap.CompressFormat.PNG, 100, fOs);
+            fOs.flush();
+            fOs.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+        ContentValues values = new ContentValues();
+
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.MediaColumns.DATA, imgPath.getPath());
+
+        getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+    }
 }
