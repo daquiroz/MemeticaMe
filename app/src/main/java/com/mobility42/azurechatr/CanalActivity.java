a88843299c41183a972014600b74d18e950aab1c
daquiroz <daquiroz@uc.cl>
2015-12-08 13:47:37 -0300

    commit dani

--- app/src/main/java/com/mobility42/azurechatr/CanalActivity.java
+++ app/src/main/java/com/mobility42/azurechatr/CanalActivity.java
@@ -18,31 +18,32 @@ import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
 import com.microsoft.windowsazure.mobileservices.MobileServiceTable;
 import com.microsoft.windowsazure.mobileservices.ServiceFilterResponse;
 import com.microsoft.windowsazure.mobileservices.TableQueryCallback;
+import com.mobility42.azurechatr.*;
 
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Dani on 07-12-15.
  */
-public class CanalActivity extends Activity {
+public class CanalActivity extends Activity{
 
-    MemeChannelAdapter memechannelAdapter;
+
+    MemeChannelAdapter memeadapter;
     ListView listviewmemes;
     private ArrayList<Meme> listaMemes;
-    public String miId = DB.miId;
-    public DB db;
-
+      public String miId = DB.miId;
+      public DB db;
+    String idcanal;
 
     private String AZUREMOBILESERVICES_URI = "https://memeticameapp.azure-mobile.net/";
     private String AZUREMOBILESERVICES_APPKEY = "GTjbnmDzTewswxMxsTzKSVpFjvYrbS22";
     private String AZUREPUSHNOTIFHUB_NAME = "memeticameapphub";
     private String AZUREPUSHNOTIFHUB_CNXSTRING = "Endpoint=sb://memeticameapphub-ns.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=1oxnmbuBGmEEREVPqqIhwV1ATMWMsFu2tquHaR8lPEQ=";
     private MobileServiceClient mClient;
-    private MobileServiceTable<Meme> memeTable;
-
-
+    private MobileServiceTable<Meme> memesTable;
 
 
     @Override
@@ -51,10 +52,19 @@ public class CanalActivity extends Activity {
         setContentView(R.layout.canal_meme);
 
         Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+
         db = new DB();
 
 
-        listaMemes = new ArrayList<Meme>();
+        try
+        {
+            idcanal = getIntent().getExtras().getString("idcanal");
+        }
+        catch(Exception excepcion)
+        {
+            idcanal = null;
+        }
+
 
         try {
             // Create the Mobile Service Client instance, using the provided
@@ -65,18 +75,29 @@ public class CanalActivity extends Activity {
                     this);
 
             // Get the Mobile Service Table instance to use
-            memeTable = mClient.getTable(Meme.class);
+            memesTable = mClient.getTable(Meme.class);
 
             listaMemes = new ArrayList<Meme>();
 
-            llenarTablaMeme();
+            llenarTablaMemes();
+            //listaFinal = new ArrayList<Contact>();
+
+            //mTextNewChat = (EditText) findViewById(R.id.textNewChat);
+
+            // Create an adapter to bind the items with the view
+//            mAdapter = new ChatItemAdapter(this,lista);
+//            listViewChat = (ListView) findViewById(R.id.listViewChat);
+//            listViewChat.setAdapter(mAdapter);
+
+            // Load the items from the Mobile Service //
 
             //refreshItemsFromTable();
 
-            memechannelAdapter = new MemeChannelAdapter(this, listaMemes);
+
+            memeadapter = new MemeChannelAdapter(this, listaMemes);
 
             listviewmemes = (ListView) findViewById(R.id.listviewMemes);
-            listviewmemes.setAdapter(memechannelAdapter);
+            listviewmemes.setAdapter(memeadapter);
 
 
         } catch (MalformedURLException e) {
@@ -84,54 +105,53 @@ public class CanalActivity extends Activity {
         }
 
 
-     //  listviewcontactos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-     //      @Override
-     //      public void onItemClick(AdapterView<?> parent, View view, int position,
-     //                              long id) {
+        listviewmemes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
 
-     //          Contact item = contactadapter.getItem(position);
-     //          String idcontactoseleccionado = item.getId();
+                Meme item = memeadapter.getItem(position);
+                //String idcontactos1eleccionado = item.getId();
+
+                //METODO QUE DADO UN CONTACTO BUSCA EL ID DEL CHAT QUE TENEMOS EN COMUN. EN CASO DE NO EXISTIR CREA UNO.
+                //String idchat = retornarIdChat2(item);
+
+                //Intent intent = new Intent(ListaContactosActivity.this, ChatActivity.class);
+                // intent.putExtra("idchat", idchat);
+                // startActivity(intent);
+
+                //Toast.makeText(getBaseContext(), idchat, Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
 
-     //          //METODO QUE DADO UN CONTACTO BUSCA EL ID DEL CHAT QUE TENEMOS EN COMUN. EN CASO DE NO EXISTIR CREA UNO.
-     //          String idchat = retornarIdChat2(item);
-
-     //          Intent intent = new Intent(ListaContactosActivity.this, ChatActivity.class);
-     //          intent.putExtra("idchat", idchat);
-     //          startActivity(intent);
-
-     //          Toast.makeText(getBaseContext(), idchat, Toast.LENGTH_SHORT).show();
-
-     //      }
-     //  });
 
 
-     //  ImageButton btnAddContact = (ImageButton)findViewById(R.id.btAgregarContacto);
-     //  btnAddContact.setOnClickListener(new View.OnClickListener() {
-     //      @Override
-     //      public void onClick(View v) {
-     //          Intent intent = new Intent(Intent.ACTION_INSERT,
-     //                  ContactsContract.Contacts.CONTENT_URI);
-     //          startActivity(intent);
-
-     //      }
-     //  });
     }
 
-    private synchronized void llenarTablaMeme() {
+    private synchronized void llenarTablaMemes() {
+
 
         // Get all the chat items and add them in the adapter
-        memeTable.execute(new TableQueryCallback<Meme>() {
+        memesTable.execute(new TableQueryCallback<Meme>() {
 
             public void onCompleted(List<Meme> result, int count, Exception exception, ServiceFilterResponse response) {
                 if (exception == null) {
                     listaMemes.clear();
 
                     for (Meme item : result) {
-
-                        Meme c = new Meme(item.getUrl(), item.getRanking(), item.getEtiquetas(), item.getCategoria());
-                        listaMemes.add(c);
+                        if (item.getIdcanal().equals(idcanal))
+                        {
+                            Meme c = new Meme(item.getUrl(), item.getRanking(), item.getEtiquetas(), item.getCategoria(), item.getIdcanal(), item.getNombrecanal(), item.getId());
+                            listaMemes.add(c);
+                        }
                     }
+
+                    //listaFinal = CompararListas(listaContactosCelular, listaContactosAzure);
+                    //contactadapter.notifyDataSetChanged();
+                    //listviewcontactos.setAdapter(contactadapter);
+
 
                 } else {
                     //createAndShowDialog(exception, "Error");
